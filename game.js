56b09994ea6d15dcd65a1c1977b196b4f4d456b6
misterwilson37 <73028734+misterwilson37@users.noreply.github.com>
2026-07-30 11:40:49 -0500

    Update game.js 3.4.0
    ACtual version change.

--- game.js
+++ game.js
@@ -67,7 +67,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFunctions, httpsCallable } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-functions.js";
 
-const VERSION = "3.2.0";
+const VERSION = "3.4.0";
 const DEFAULT_BOOK = "wizard_of_oz";
 const IDLE_THRESHOLD = 2000;
 const AFK_THRESHOLD = 5000; // 5 Seconds to Auto-Pause
